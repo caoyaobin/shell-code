96e87eb3b4ae55f2566b7bbda6139fb8b6c5d7c8
caoyaobin <2216314254@qq.com>
2017-10-25 20:54:02 +0800

    adding code

---
+++ comm.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: comm.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Wed 25 Oct 2017 08:51:09 PM CST
+#########################################################################
+#!/bin/bash
+time=`date "%Y%m%d"`
+git commit -m " adding code $time "
+git push
+
